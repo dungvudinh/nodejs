a3143f77a3f10adc08f19b02b6111ff1c563a5cf
Dungg <dungg@Dunggs-MacBook-Pro.local>
2023-08-11 15:21:38 +0700

    second-commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const morgan = require('morgan');
+const {engine} = require('express-handlebars');
+const path = require('path');
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public")))
+app.engine(".hbs", engine({extname:'.hbs'}));
+app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "resources/views"));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home');
 })
 app.get('/upload', (req, res)=>{
-    res.send("upload page");
+    res.render("upload");
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
